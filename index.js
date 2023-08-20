aa1d595709060a0c94b9b043bed1d547c900a285
Puri Calvo <Puri_adrian@hotmail.com>
2023-08-20 17:10:25 +0200

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de Express
+const app = express();
+
+// Configurar Cors
+app.use( cors() );
+
+// Base de datos
+dbConnection();
+
+
+
+// Rutas
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+
+});
+
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
+});
+
+
+
+
